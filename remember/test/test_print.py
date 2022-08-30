b88bb0b4c2ee2f463f8e551f2d116d096f9fe72f
Seth Bashford <seth.bashford@proton.me>
2022-08-30 11:19:59 -0500

    add to test

--- remember/test/test_print.py
+++ remember/test/test_print.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime
+
 from remember.these import Item, Time, new, add
 
 
@@ -13,16 +15,21 @@ class FileStub:
 class PrintTestCase(unittest.TestCase):
     def test_tbd(self):
         items = new()
-        add(items, Item(description="replace water filter", time=Time(months=2)))
-        add(items, Item(description="change toothbrush", time=Time(months=6)))
-        add(items, Item(description="change oil", time=Time(months=4)))
+        fromDate = datetime.date(2022, 8, 30)
+        add(
+            items,
+            Item(description="replace water filter", time=Time(months=2)),
+            fromDate,
+        )
+        add(items, Item(description="change toothbrush", time=Time(months=6)), fromDate)
+        add(items, Item(description="change oil", time=Time(months=4)), fromDate)
         file = FileStub()
         print(items, file=file)
         self.assertEqual(
             file.written,
             """\
-in 2 months replace water filter
-in 4 months change oil
-in 6 months change toothbrush
+October 30, 2022 - replace water filter
+December 30, 2022 - change oil
+February 28, 2023 - change toothbrush
 """,
         )
--- remember/these.py
+++ remember/these.py
@@ -1,4 +1,6 @@
 from typing import NamedTuple, List
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 import bisect
 
 
@@ -18,17 +20,35 @@ class Item(NamedTuple):
         return self.time.months < other.time.months
 
 
-class Items:
+class ToDo(NamedTuple):
+    description: str
+    date: date
+
+    def __lt__(self, other):
+        return self.date < other.date
+
+    def __repr__(self):
+        return "{} - {}".format(self.date.strftime("%B %d, %Y"), self.description)
+
+
+class Remember:
     def __init__(self):
-        self.items: List[Item] = []
+        self.todos: List[ToDo] = []
 
     def __str__(self):
-        return ("{}" + "\n{}" * (len(self.items) - 1)).format(*self.items)
+        return ("{}" + "\n{}" * (len(self.todos) - 1)).format(*self.todos)
 
 
-def new() -> Items:
-    return Items()
+def new() -> Remember:
+    return Remember()
 
 
-def add(items: Items, item: Item):
-    bisect.insort(items.items, item)
+def create_todo(item: Item, fromDate: date) -> ToDo:
+    return ToDo(
+        description=item.description,
+        date=fromDate + relativedelta(years=item.time.years, months=item.time.months),
+    )
+
+
+def add(items: Remember, item: Item, fromDate: date):
+    bisect.insort(items.todos, create_todo(item, fromDate))
