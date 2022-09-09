71cca281d8dbdd1f19ac4ab2c581ecc1ed727907
Seth Bashford <seth.bashford@proton.me>
2022-09-09 14:17:43 -0500

    add test

--- maintenance_schedule/parse.py
+++ maintenance_schedule/parse.py
@@ -3,6 +3,20 @@ import json
 from maintenance_schedule.remind import Maintenance, HowOften
 
 
+def to_int(s: str) -> int:
+    try:
+        return int(s)
+    except ValueError:
+        return 0
+
+
 def parse_maintenance(message: str) -> Maintenance:
     decoded = json.loads(message)
-    return Maintenance(what=decoded["what"], how_often=HowOften())
+    return Maintenance(
+        what=decoded["what"],
+        how_often=HowOften(
+            days=to_int(decoded["days"]),
+            months=to_int(decoded["months"]),
+            years=to_int(decoded["years"]),
+        ),
+    )
--- maintenance_schedule/test/test_parse.py
+++ maintenance_schedule/test/test_parse.py
@@ -7,6 +7,9 @@ from maintenance_schedule.parse import parse_maintenance
 class ParseTestCase(unittest.TestCase):
     def test_tbd(self):
         maintenance = parse_maintenance(
-            '{"method":"add","what":"wash vacuum filters","months":"2","years":""}'
+            '{"method":"add","what":"wash vacuum filters","days":"","months":"2","years":""}'
         )
         self.assertEqual(maintenance.what, "wash vacuum filters")
+        self.assertEqual(maintenance.how_often.days, 0)
+        self.assertEqual(maintenance.how_often.months, 2)
+        self.assertEqual(maintenance.how_often.years, 0)
