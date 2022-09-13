d9eb280802da3f0a3bac82fed02436bfa2686703
Seth Bashford <seth.bashford@proton.me>
2022-09-13 15:36:27 -0500

    refactor

--- maintenance_schedule/parse.py
+++ maintenance_schedule/parse.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import json
 
 from maintenance_schedule.remind import Maintenance, HowOften
@@ -10,13 +11,17 @@ def to_int(s: str) -> int:
         return 0
 
 
+def parse_how_often(d: Dict[str, int]) -> HowOften:
+    return HowOften(
+        days=to_int(d["days"]),
+        months=to_int(d["months"]),
+        years=to_int(d["years"]),
+    )
+
+
 def parse_maintenance(message: str) -> Maintenance:
     decoded = json.loads(message)
     return Maintenance(
         what=decoded["what"],
-        how_often=HowOften(
-            days=to_int(decoded["howOften"]["days"]),
-            months=to_int(decoded["howOften"]["months"]),
-            years=to_int(decoded["howOften"]["years"]),
-        ),
+        how_often=parse_how_often(decoded["howOften"]),
     )
